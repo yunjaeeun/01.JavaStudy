17741cd33d28f246dad1b469250fd8e93098cf46
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-01-23 22:00:56 +0900

    24.01.23 과제
    로또번호 난수생성

--- study02-Array/src/main/java/section01/lotto/lotto.java
+++ study02-Array/src/main/java/section01/lotto/lotto.java
@@ -6,18 +6,26 @@ public class lotto {
     public static void main(String[] args) {
 
         int[] arr = new int[6];
-        int numLotto = (int)(Math.random() * 45 + 1);
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(numLotto);
-//        }
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int)(Math.random() * 45 + 1);
-            System.out.println(arr[i]);
+            int lotto;
+            boolean isTrue;
+
+            do {
+                lotto = (int)(Math.random() * 45 +1);
+                isTrue = false;
+                for (int j = 0; j < i; j++) {
+                    if (arr[j] == lotto){
+                        isTrue = true;
+                        break;
+                    }
+                }
+            } while (isTrue);
+            arr[i] = lotto;
         }
 
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);                                   // 정렬
+        System.out.println(Arrays.toString(arr));           // 배열 출력
 
     }
 }
