4dd217f9c35667fd5c2343d136e4f505cc16a928
yunjaeeun <yunjaeeun12@naver.com>
2024-01-23 17:40:00 +0900

    24.01.23 공부내용
    난수생성, 정렬추가

--- study02-Array/src/main/java/section01/lotto/lotto.java
+++ study02-Array/src/main/java/section01/lotto/lotto.java
@@ -1,14 +1,23 @@
 package section01.lotto;
 
+import java.util.Arrays;
+
 public class lotto {
     public static void main(String[] args) {
 
         int[] arr = new int[6];
         int numLotto = (int)(Math.random() * 45 + 1);
-        System.out.println(arr.length);
-        System.out.println(arr.toString());
 //        for (int i = 0; i < arr.length; i++) {
 //            System.out.println(numLotto);
 //        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 45 + 1);
+            System.out.println(arr[i]);
+        }
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
