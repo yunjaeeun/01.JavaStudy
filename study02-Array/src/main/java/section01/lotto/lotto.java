e769e843427389c6fe38c34934a4ada850ad8111
yunjaeeun <yunjaeeun12@naver.com>
2024-01-23 17:29:40 +0900

    23.01.23 공부내용
    배열을 이용해서 로또번호생성기 제작

---
+++ study02-Array/src/main/java/section01/lotto/lotto.java
@@ -0,0 +1,14 @@
+package section01.lotto;
+
+public class lotto {
+    public static void main(String[] args) {
+
+        int[] arr = new int[6];
+        int numLotto = (int)(Math.random() * 45 + 1);
+        System.out.println(arr.length);
+        System.out.println(arr.toString());
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(numLotto);
+//        }
+    }
+}
