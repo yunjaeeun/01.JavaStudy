7a091a948a988eb261587cd1ea53bd3a6082aa43
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-01-29 21:39:44 +0900

    24.01.29 공부내용
    계산결과에 따라 문자열을 더하는 코드 작성

---
+++ study01-control/src/main/java/study/ifSwitch/Problem/Baek/No25314.java
@@ -0,0 +1,19 @@
+package study.ifSwitch.Problem.Baek;
+
+import java.util.Scanner;
+
+public class No25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        String result = "";
+
+
+        for (int i = 0; i < input; i++) {
+            if (i % 4 == 0){
+                result = (result + "long ");
+            }
+        }
+        System.out.println(result + "int");
+    }
+}
