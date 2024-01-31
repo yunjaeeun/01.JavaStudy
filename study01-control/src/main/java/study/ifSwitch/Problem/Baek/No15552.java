846bf7cf67a01ca7741494bd1b08dc0b257f0475
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-01-31 21:03:44 +0900

    24.01.31 공부내용
    반복문을 이용한 별찍기

--- study01-control/src/main/java/study/ifSwitch/Problem/Baek/No15552.java
+++ study01-control/src/main/java/study/ifSwitch/Problem/Baek/No15552.java
@@ -1,11 +1,27 @@
 package study.ifSwitch.Problem.Baek;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class No15552 {
-    public static void main(String[] args) {
-        BufferedReader br = null;
-        br.
+    static int repeat;
+
+    public static BufferedReader toBufferedReader(String str) {
+        InputStream is = new ByteArrayInputStream(str.getBytes());
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String input = "";
+        BufferedReader br = toBufferedReader(input);
+        repeat = Integer.valueOf(br.readLine());
+        for (int i = 0; i < repeat; i++) {
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            int a = Integer.valueOf(str.nextToken());
+            int b = Integer.valueOf(str.nextToken());
+            int result = a + b;
+            System.out.println(result);
+        }
     }
 }
---
+++ study01-control/src/main/java/study/ifSwitch/Problem/Baek/No2438.java
@@ -0,0 +1,18 @@
+package study.ifSwitch.Problem.Baek;
+
+import java.util.Scanner;
+
+public class No2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
