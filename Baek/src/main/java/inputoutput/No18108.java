6b30d46160c490cb9065854fe267edeac9819b61
yunjaeeun <yunjaeeun12@naver.com>
2024-02-10 13:46:56 +0900

    24.02.10 공부내용
    자바 입출력 에제

---
+++ Baek/src/main/java/inputoutput/No18108.java
@@ -0,0 +1,12 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class No18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a - 543);
+    }
+}
