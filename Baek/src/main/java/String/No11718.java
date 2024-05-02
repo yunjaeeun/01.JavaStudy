ed1b6c25f4e795a385202046aa8d013a826cfba2
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-05-02 21:20:20 +0900

    24.05.02 공부내용
    입력받은 문자열 그대로 출력

--- Baek/src/main/java/String/No11718.java
+++ Baek/src/main/java/String/No11718.java
@@ -2,9 +2,13 @@ package String;
 
 import java.util.Scanner;
 
+/* 문자열 출력 */
 public class No11718 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
     }
 }
