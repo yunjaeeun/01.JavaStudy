5fcbbc86d7e2867523796c0d86da74e6a5ca6619
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-05-05 22:49:37 +0900

    24.05.03 공부내용
    새싹 출력

---
+++ Baek/src/main/java/String/No25083.java
@@ -0,0 +1,13 @@
+package String;
+
+/* 새싹 출력 */
+public class No25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
